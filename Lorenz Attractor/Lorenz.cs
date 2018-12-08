d273496b9cbe677f679b3d56c09e6771ff49a9f8
JPBotelho <worldoftanksreviewer@gmail.com>
2018-12-08 23:45:55 +0000

    Stable release, code is working. Making improvements and testing new formulas.

---
+++ Lorenz Attractor/Lorenz.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lorenz_Attractor
+{
+	class Lorenz
+	{
+		public static int sizeX = 2000, sizeY = 2000;
+		public static double a = 10, b = 28, c = 8 / 3;
+		public static double timeStep = 0.001;
+		public static int iterations = 200000;
+		public double x = 0.1, y = 0, z = 0;
+
+		private int[] heatmap;
+		public static int biggestHit = 0;
+
+		public Lorenz()
+		{
+			heatmap = new int[sizeX * sizeY];
+		}
+
+		//y * width + x
+		public int[] Render()
+		{
+			for (int i = 0; i < iterations; i++)
+			{
+				x += timeStep * a * (y - x);
+				y += timeStep * (x * (b - z) - y);
+				z += timeStep * (x * y - c * z);
+
+				ProcessPoint(x, y, z);
+				
+				//Console.WriteLine("X: " + Math.Round(x*100) + " Y: " + Math.Round(y*100));
+			}
+			Console.WriteLine(t + " points escaped.");
+
+			return heatmap;
+		}
+		static int t;
+		private void ProcessPoint(double x, double y, double z)
+		{
+			int xPixel = (int)Math.Round(x * 40) + sizeX/2;
+			int yPixel = (int)Math.Round(y * 40) + sizeY/2;
+			int zPixel = (int)Math.Round(z * 40) + sizeY/2;
+			if (xPixel <= 0 || xPixel >= sizeX)
+			{
+				t++;
+				return;
+			}
+			if (yPixel <= 0 || yPixel >= sizeY)
+			{
+				t++;
+				return;
+			}
+
+			heatmap[yPixel * sizeX + xPixel]++;
+
+			if(heatmap[yPixel * sizeX + xPixel] > biggestHit)
+				biggestHit = heatmap[yPixel * sizeX + xPixel];
+		}
+	}
+}
--- Lorenz Attractor/Program.cs
+++ Lorenz Attractor/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
 namespace Lorenz_Attractor
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			int[] info;
+			Lorenz lorenz = new Lorenz();
+			info = lorenz.Render();
+
+			Bitmap bitmap = new Bitmap(Lorenz.sizeX, Lorenz.sizeY);
+			for(int height = 0; height < Lorenz.sizeY; height++)
+			{
+				for (int width = 0; width < Lorenz.sizeX; width++)
+				{
+					int hitCount = Math.Min(255, info[height * Lorenz.sizeX + width]);
+					Color color;
+					if (hitCount != 0)
+					{
+						color = Color.FromArgb(255, (int)hitCount, (int)hitCount, (int)hitCount);
+					}
+					else
+					{
+						color = Color.FromArgb(255, 255, 255, 255);
+					}
+
+					bitmap.SetPixel(width, height, color);
+				}
+			}
+			Console.WriteLine("Biggest hit: "+Lorenz.biggestHit);
+			bitmap.Save("image.png");
+			Console.WriteLine("Exported.");
+			Console.ReadKey();
+			System.Diagnostics.Process.Start("image.png");
+
 		}
 	}
 }
